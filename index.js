15c1f94b438f76fcf446c27da0ed0f97883554d2
nathansmith <nathan@sonspring.com>
2016-05-18 10:23:23 -0500

    Removing "main" from package.json and creating a root index.js file, so that Webpack properly builds non-React projects. Related ticket… https://github.com/nathansmith/unsemantic/pull/90

---
+++ index.js
@@ -0,0 +1,21 @@
+/*global
+exports,
+require
+*/
+
+/*
+  // This file exists for use with NPM and React.
+  // It is not meant to be loaded into a browser.
+
+  // Example...
+
+  import { Grid, GridContainer } from 'unsemantic'
+*/
+
+exports.Grid = require('./assets/react/grid')
+
+exports.GridClear = require('./assets/react/grid_clear')
+
+exports.GridContainer = require('./assets/react/grid_container')
+
+exports.GridOffset = require('./assets/react/grid_offset')
